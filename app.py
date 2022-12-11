4cc3e03816754e8371461a705f429a1df434880a
Matt Felton <matt.felton@feltonma.com>
2022-12-11 16:47:48 -0500

    modified - added functionality
    * Added functionality to return a secret from Key Vault

--- app.py
+++ app.py
@@ -1,30 +1,74 @@
 import logging
 import json
-import requests
 import os
+import requests
+from sys import exc_info
 from flask import Flask, render_template, request, redirect
+from opencensus.ext.azure.log_exporter import AzureLogHandler
+from azure.identity import DefaultAzureCredential
+from azure.keyvault.secrets import SecretClient
+
+# Setup logging mechanism
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+)
+logger = logging.getLogger(__name__)
+logger.addHandler(AzureLogHandler(
+    connection_string=os.getenv('APPLICATIONINSIGHTS_CONNECTION_STRING'))
+)
 
 # Setup up a Flask instance
 app = Flask(__name__)
 
-# Retrieve the time from an open API
-def retrieve_time():
+# Obtain time from public api
+def query_time():
+    response = requests.get(
+        url="http://worldclockapi.com/api/json/utc/now"
+    )
+
+    if response.status_code == 200:
+        time = (json.loads(response.text))['currentDateTime']
+        logging.info('Successfully queried public API')
+        return time
+    else:
+        raise Exception(
+            f"Failed to query time API. Status code was: {response.status_code}")
+        logger.error(
+            f"Error querying API.  Status code: {response.status_code}")
+
+# Get Key Vault secret
+def get_secret():
+
+    VAULT_NAME = os.getenv('KEY_VAULT_NAME')
+    KEY_VAULT_SECRET_NAME = os.getenv('KEY_VAULT_SECRET_NAME')
     try:
-        response = requests.get(
-            url="http://worldclockapi.com/api/json/utc/now"
-        )
-        if response.status_code == 200:
-            time = (json.loads(response.text))['currentDateTime']
-            return time
+        if 'MSI_CLIENT_ID':
+            credential = DefaultAzureCredential(
+                managed_identity_client_id=os.getenv('MSI_CLIENT_ID')
+            )
         else:
-            logging.error(f"Error querying API.  Status code: {response.status_code}")
+            raise Exception
     except Exception:
-        return "Retrieving time failed"
+        raise Exception(
+            'Failed to obtain access token'
+        )
+        logger.error('Failed to obtain access token', exc_info=True)
+    try:
+        secret_client = SecretClient(
+            vault_url=f"https://{VAULT_NAME}.vault.azure.net/", credential=credential)
+        secret = secret_client.get_secret(f"{KEY_VAULT_SECRET_NAME}")
+        return secret.value
+    except Exception:
+        raise Exception(
+            'Failed to get secret'
+        )
+        logger.error('Failed to get secret', exc_info=True)
+
 
 # Render the template
 @app.route("/")
 def index():
-    return render_template('index.html')
-
-# Make functions available to web template
-app.jinja_env.globals.update(retrieve_time=retrieve_time)
+    wordoftheday = get_secret()
+    todaystime = query_time()
+    return render_template('index.html', wordoftheday=wordoftheday, time=todaystime)
